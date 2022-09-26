9c9fcd541a720070303ca336e8b5d5d2b97ac6fa
Mohamed Ayman <mohayman2007@gmail.com>
2022-09-26 21:00:42 +0000

    Project 0x03 - Task 14 - CPython #0: Python lists

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <object.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p)
+{
+	if (PyList_Check(p))
+	{
+		printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
+		printf("[*] Allocated = %lu\n", ((PyListObject *)(p))->allocated);
+
+		for (int i = 0 ; i < PyList_Size(p) ; i++)
+			printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+	}
+}
