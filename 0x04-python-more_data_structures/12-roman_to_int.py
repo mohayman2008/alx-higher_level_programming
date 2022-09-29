6ff66ec5105ba38ea9bed039ce84d894cb429e94
Mohamed Ayman <mohayman2007@gmail.com>
2022-09-29 01:14:46 +0000

    Project 0x04 - Task 12 - "Roman to Integer"

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -26,19 +26,3 @@ def roman_to_int(roman_string):
             num -= digits.get(digit)
         i += 1
     return num
-
-
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
