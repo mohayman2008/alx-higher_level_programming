79afc7102c8984127502f08d564f0f5271448413
Mohamed Ayman <mohayman2007@gmail.com>
2022-09-29 00:25:18 +0000

    Project 0x04 - Tasks 0 --> 11

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if type(a_dictionary) is not dict:
+        return None
+
+    max = None
+    key_max = None
+    for k, v in a_dictionary.items():
+        if max == None or v > max:
+            max = v
+            key_max = k
+    return key_max
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+This folder contain the solutions for the tasks of project "0x04. Python - More Data Structures: Set, Dictionary"
