fdedb34961fbca57533f19e6df618ed9782c0820
Mohamed Ayman <mohayman2007@gmail.com>
2022-10-10 21:30:31 +0000

    Project 0x05 - Mandatory Tasks 0 --> 6

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    i = 0
+    new = []
+    for i in range(list_length):
+        result = 0
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+        except (ValueError, TypeError):
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        finally:
+            new.append(result)
+    return new
