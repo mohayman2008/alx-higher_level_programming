0e3143ff8c4c2a5ac4301d8729c6101fb93a4b00
Mohamed Ayman <mohayman2007@gmail.com>
2022-10-10 22:34:10 +0000

    Project 0x05 - Mandatory Tasks 0 --> 6

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,5 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
         except (ValueError, TypeError):
             pass
+        else:
+            printed += 1
     print()
     return printed
