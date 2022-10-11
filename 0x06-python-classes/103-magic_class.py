1e3d94c58daaa2079a5c340436e4fd270d225b56
Mohamed Ayman <mohayman2007@gmail.com>
2022-10-11 18:43:03 +0000

    Project 0x06 - Task 10 - "ByteCode -> Python #5"

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This module contains the definition of the class MagicClass
+"""
+import math
+
+
+class MagicClass:
+    """MagicClass
+    """
+    def __init__(self, radius):
+        """Initialize the MagicClass object
+        """
+        self.__radius = 0
+
+        if not isinstance(radius, int) or not isinstance(radius, float):
+            raise TypeError("radius must be a number")
+        else:
+            self.__radius = radius
+
+    def area(self):
+        """Returns the area of the MagicClass
+        """
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self):
+        """Returns the circumference of the MagicClass
+        """
+        return 2 * math.pi * self.__radius
+    pass
