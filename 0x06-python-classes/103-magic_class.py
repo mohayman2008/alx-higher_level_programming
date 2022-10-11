ddce44d0ce616985ff7100cc37c32d7c6b62dc4a
Mohamed Ayman <mohayman2007@gmail.com>
2022-10-11 18:54:07 +0000

    Project 0x06 - Task 10 - "ByteCode -> Python #5"

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -13,6 +13,7 @@ class MagicClass:
         self.__radius = 0
 
         if not isinstance(radius, int) or not isinstance(radius, float):
+            # if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         else:
             self.__radius = radius
