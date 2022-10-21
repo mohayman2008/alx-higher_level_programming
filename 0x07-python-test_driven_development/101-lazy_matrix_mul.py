6d64ce580d9a8d456aee4b5bcc8cebf360c7dabd
Mohamed Ayman <mohayman2007@gmail.com>
2022-10-21 09:07:10 +0000

    Project 0x07 - Advanced Tasks 6 --> 8"

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -7,7 +7,6 @@ import numpy as np
 def lazy_matrix_mul(m_a, m_b):
     """Multiplies 2 matrices using NumPy"""
 
-    m_a, m_b = (np.array(m_a), np.array(m_b))
+    # m_a, m_b = (np.array(m_a), np.array(m_b))
 
-    result = np.matmul(m_a, m_b)
-    return result
+    return np.matmul(m_a, m_b)
