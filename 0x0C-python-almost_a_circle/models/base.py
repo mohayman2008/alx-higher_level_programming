38d5c63e39a544b841aafc3796e78223fff0eeb7
Mohamed Ayman <mohayman2007@gmail.com>
2022-10-31 21:24:47 +0000

    Project 0x0C - Mandatory Tasks - 10 --> 19"

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,14 +27,14 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of a list of dictionaries"""
-        # if (type(list_dictionaries) is not list)
-        # or not len(list_dictionaries):
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        # try:
         return json.dumps(list_dictionaries)
-        # except TypeError:
-        #     return json.dumps([])
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+        """
 
     @staticmethod
     def from_json_string(json_string):
