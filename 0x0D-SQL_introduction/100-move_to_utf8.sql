ec95d9ffc3254777a5531900d3ffc7fc88add67b
Mohamed Ayman <mohayman2007@gmail.com>
2022-11-30 02:58:29 +0000

    Project 0x0D - Advanced Tasks 17 --> 20

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,14 @@
 -- This script creates the database hbtn_0c_0 in the MySQL server if it's not existing
 
 -- Creates the database hbtn_0c_0 in the MySQL server if it's not existing
-ALTER DATABASE hbtn_0c_0
+ALTER DATABASE `hbtn_0c_0`
 CHARSET=utf8mb4 
 COLLATE=utf8mb4_unicode_ci;
 
 -- Creates the database hbtn_0c_0 in the MySQL server if it's not existing
-ALTER TABLE first_table
+ALTER TABLE `first_table`
 CHARSET=utf8mb4 
 COLLATE=utf8mb4_unicode_ci;
+
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
