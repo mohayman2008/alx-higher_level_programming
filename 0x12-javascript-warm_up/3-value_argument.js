8f9521ca53b3a5df01907b63c06b00f389988e39
Mohamed Ayman <mohayman2007@gmail.com>
2022-12-20 02:08:19 +0000

    Project 0x12 - Mandatory Tasks 0 --> 6

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const argv = require('node:process').argv;
+
+if (typeof argv[2] === 'undefined') {
+  console.log('No argument');
+} else {
+  console.log(argv[2]);
+}
---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+const argv = require('node:process').argv;
+
+console.log(argv[2] + ' is ' + argv[3]);
