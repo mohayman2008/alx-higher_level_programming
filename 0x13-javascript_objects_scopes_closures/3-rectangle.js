22c9b933954410c8e06a1ef27b1f7fee6d6f3d6d
Mohamed Ayman <mohayman2007@gmail.com>
2022-12-20 22:18:37 +0000

    Project 0x13 - Mandatory Tasks 0 --> 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w && h && Number(w) > 0 && Number(h) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (!this.height || !this.width) {
+      return;
+    }
+
+    let out = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        out += 'X';
+      }
+      if (i < this.height - 1) {
+        out += '\n';
+      }
+    }
+    console.log(out);
+  }
+};
