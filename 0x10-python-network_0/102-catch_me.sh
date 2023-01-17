c747805f73b6110d4170a32a64265bd9354274bd
Mohamed Ayman <mohayman2007@gmail.com>
2023-01-17 01:53:30 +0000

    Project 0x10 - Advanced Task 9 - "Catch me if you can!"

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me to get "You got me!" in the body of the response.
-curl -L -X 'PUT' -d 'user_id=98' -H 'Origin: School' 0.0.0.0:5000/catch_me
+curl -L -X 'PUT' -d 'user_id=98' -H 'Origin: School' 0.0.0.0:5000/catch_me_2
