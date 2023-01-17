99652709d44cb8f293ba3c8a1a806a24bbdd1c16
Mohamed Ayman <mohayman2007@gmail.com>
2023-01-17 02:29:49 +0000

    Project 0x10 - Task 6 - "Find a peak"

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,13 +12,18 @@ def find_peak(list_of_integers):
     if length == 1:
         return lst[0]
     if length == 2:
-        if lst[0] >= lst[1]:
-            return lst[0]
-        return lst[1]
+        return lst[0] if lst[1] < lst[0] else lst[1]
 
     center = int(length / 2)
-    if lst[center - 1] <= lst[center] > lst[center + 1]:
+    if lst[center - 1] < lst[center] > lst[center + 1]:
         return lst[center]
-    if lst[center - 1] >= lst[center + 1]:
-        return find_peak(lst[:center])
-    return find_peak(lst[center:])
+    if lst[center + 1] > lst[center - 1]:
+        return find_peak(lst[center:])
+    return find_peak(lst[:center + 1])
+
+print(find_peak([1, 2, 4, 6, 3]))
+print(find_peak([4, 2, 1, 2, 3, 1]))
+print(find_peak([2, 2, 2]))
+print(find_peak([]))
+print(find_peak([-2, -4, 2, 1]))
+print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
