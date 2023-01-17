7ead58d03aa636b00608890eb41805381c083dcd
Mohamed Ayman <mohayman2007@gmail.com>
2023-01-17 00:25:55 +0000

    Project 0x10 - Task 6 - "Find a peak"

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''This module contains the definition of "find_peak" function'''
+
+
+def find_peak(list_of_integers):
+    '''Finds a peak in a list of unsorted integers'''
+    if not isinstance(list_of_integers, list) or not list_of_integers:
+        return None
+    lst = list_of_integers
+    length = (len(lst))
+    if length == 1:
+        return lst[0]
+    if length == 2:
+        if lst[0] >= lst[1]:
+            return lst[0]
+        return lst[1]
+
+    center = int(length / 2)
+    if lst[center - 1] < lst[center] > lst[center + 1]:
+        return lst[center]
+    if lst[center - 1] > lst[center + 1]:
+        return find_peak(lst[:center])
+    return find_peak(lst[center+1:])
