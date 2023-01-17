a1b8a8960c51013cfb4fb80e7cbd245f91b0641c
Mohamed Ayman <mohayman2007@gmail.com>
2023-01-17 20:39:23 +0000

    Project 0x11 - Tasks 8 & 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,23 +5,20 @@ from sys import argv
 
 import requests
 HTTPBasicAuth = requests.auth.HTTPBasicAuth
-JSONDecodeError = requests.JSONDecodeError
 
 
 def main():
     '''Main function'''
-    data = {'q': ''}
     if len(argv) < 3:
         print("None")
         return
 
     auth = HTTPBasicAuth(argv[1], argv[2])
     response = requests.get('https://api.github.com/user', auth=auth)
-    # response = requests.get('https://alx-intranet.hbtn.io/status', auth=auth)
     try:
         id = response.json().get('id', None)
         print(id)
-    except (JSONDecodeError, ValueError):
+    except ValueError:
         print("Not a valid JSON")
 
 
